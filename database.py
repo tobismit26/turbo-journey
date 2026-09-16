c31e7f2b648164e79530b00f3703b576aa49ca1f
tobismit26 <tobismit26@gmail.com>
2026-09-16 23:01:22 +0300

    changes1

--- database.py
+++ database.py
@@ -1,10 +1,36 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base
+from sqlalchemy import Column, DateTime, func
 
-def create_engine():
-    Base = declarative_base()
-    engine = create_engine("postgresql://postgres:3077@localhost:5432/db1")
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+    data = Column( DateTime, default=func.now())
+
+load_dotenv() 
+
+def create_my_engine():
+    DB_URL = os.getenv("DATABASE")
+    if not DB_URL:
+        raise EnvironmentError("Переменная не найдена")
+    
+    engine = create_engine(DB_URL)
     Base.metadata.create_all(engine)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     session = SessionLocal()
+    return session
+
+def read_table(session):
+    user = session.query(User).all()
+    for i in user:
+        print(i.name)
+
+read_table(create_my_engine())
 
