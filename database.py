72fe911e40fc97191a019c666a99ae3b095126ff
tobismit26 <tobismit26@gmail.com>
2026-09-17 21:31:23 +0300

    changes2

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship, declarative_base
-from sqlalchemy import Column, DateTime, func
+from pydantic import BaseModel, EmailStr
 
 Base = declarative_base()
 
@@ -12,10 +12,24 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     email = Column(String)
-    data = Column( DateTime, default=func.now())
+
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+
+class UserProfile:
+    def create_or_update(self, session, name:str, email:str) -> UserCreate:
+        user = session.query(User).filter(User.name.like(f'{name}%')).all()
+       # if user
+        print(user[0].name)
+        for i in user:
+            print(i.name)
+
+
+
 
 load_dotenv() 
-
+#подключение к бд
 def create_my_engine():
     DB_URL = os.getenv("DATABASE")
     if not DB_URL:
@@ -32,5 +46,8 @@ def read_table(session):
     for i in user:
         print(i.name)
 
-read_table(create_my_engine())
+session = create_my_engine()
+#read_table(session)
 
+aw = UserProfile()
+aw.create_or_update(session, 'one', 'saha@gmail.com')
